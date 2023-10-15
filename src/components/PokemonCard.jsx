ed1104865657ae8ba59ed818a01cfaeb7c61d205
hipticem <maamir.hipticem@gmail.com>
2023-10-15 22:20:55 +0200

    quete 3

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,11 +1,31 @@
-function PokemonCard (){
-    return (
-    <figure>
-<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"/>
-    <figcaption>
-        "Bulbasaur"
-    </figcaption>
-    </figure>
-    )
-}
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+]
+
+  function PokemonCard() {
+    const pokemon = pokemonList[0];
+    console.log(pokemon.imgSrc)
+
+    return <figure>
+        {pokemon.imgSrc ? <img src={pokemon.imgSrc} />: <p>???</p>}
+
+            <figcaption>
+                {pokemon.name}
+             </figcaption>
+    </figure>;
+
+  }
+
+ 
+    
+
+
+
 export default PokemonCard;
