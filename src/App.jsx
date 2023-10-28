34460bbba86003f1cecd0fc18a95d29fd169861d
hipticem <maamir.hipticem@gmail.com>
2023-10-28 23:07:47 +0200

    state 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
 import PokemonCard from "./components/PokemonCard";
 import "./app.css";
+import { useState } from "react";
 
-function Card (){
-  pokemonList
-  
+function Card() {
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+
+  const handleClickSuivant = () => {
+    if (pokemonIndex < pokemonList.length - 1) {
+      setPokemonIndex(pokemonIndex + 1);
+    }
+  };
+  const handleClickPrecedent = () => {
+    if (pokemonIndex > 0) {
+      setPokemonIndex(pokemonIndex - 1);
+    }
+  };
+
   return (
-  <div>
- 
-  <PokemonCard pokemon={pokemonList[0]}/>
+    <div>
+      <button onClick={handleClickPrecedent} disabled={pokemonIndex === 0}>
+        precedent
+      </button>
+      <button
+        onClick={handleClickSuivant}
+        disabled={pokemonIndex === pokemonList.length - 1}
+      >
+        suivant
+      </button>
+
+      <PokemonCard pokemon={pokemonList[pokemonIndex]} />
     </div>
-    )
+  );
 }
 
 const pokemonList = [
@@ -19,8 +40,23 @@ const pokemonList = [
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
     name: "mew",
   },
-]
-export default Card;
+];
 
+export default Card;
